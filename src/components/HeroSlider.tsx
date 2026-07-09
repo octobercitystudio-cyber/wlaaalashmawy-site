1055eb7f84f164c9659c766901ae81c4b88775ec
Alashmawy Admin <admin@alashmawy.com>
2026-07-10 00:32:48 +0300

    Shift hero text block upwards

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -81,7 +81,7 @@ export default function HeroSlider() {
         position: "relative",
         width: "100%"
       }}>
-        <div style={{ maxWidth: "800px", width: "100%", textAlign: "center", marginTop: "8rem" }}>
+        <div style={{ maxWidth: "800px", width: "100%", textAlign: "center", marginBottom: "4rem" }}>
           
           <h1 style={{ 
             color: "#FFFFFF", 
